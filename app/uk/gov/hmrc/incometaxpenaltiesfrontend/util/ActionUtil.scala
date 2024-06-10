72d35c1ed2400a8b7648a6631909c25f625c3653
Dan Boresjö <6451217+dboresjo@users.noreply.github.com>
2024-06-10 12:24:43 +0100

    action util to enable index semantics on urls by enforcing a trailing slash

---
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/util/ActionUtil.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxpenaltiesfrontend.util
+
+import play.api.Logging
+import play.api.mvc.Results.Redirect
+import play.api.mvc.{ActionBuilder, BodyParser, Request, Result}
+
+import java.net.URI
+import scala.concurrent.{ExecutionContext, Future}
+
+object ActionUtil extends Logging {
+  implicit class ActionBuilderPlus[+R[_], B] (inner: ActionBuilder[R,B]) {
+    def asIndex(implicit ec: ExecutionContext): ActionBuilder[R,B] = new ActionBuilder[R,B] {
+      override def parser: BodyParser[B] = inner.parser
+
+      override def invokeBlock[A](request: Request[A], block: R[A] => Future[Result]): Future[Result] = {
+        if (request.path.endsWith("/")) {
+          inner.invokeBlock(request, block)
+        } else {
+          Future.successful(Redirect(addTrailingSlash(request.uri)))
+        }
+      }
+
+      override protected def executionContext: ExecutionContext = ec;
+    }
+  }
+
+  def addTrailingSlash(uri: String): String = {
+    val originalURI = URI.create(uri)
+    if (originalURI.getRawPath.endsWith("/")) {
+      uri
+    } else {
+      val redirectTo = originalURI.getScheme + "://" + originalURI.getRawAuthority + originalURI.getRawPath + "/" +
+        (if (Option(originalURI.getRawQuery).nonEmpty) "?" + originalURI.getRawQuery else "") +
+        (if (Option(originalURI.getRawFragment).nonEmpty) "#" + originalURI.getRawFragment else "")
+
+      redirectTo
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/incometaxpenaltiesfrontend/util/ActionUtilSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxpenaltiesfrontend.util;
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import uk.gov.hmrc.incometaxpenaltiesfrontend.util.ActionUtil.addTrailingSlash
+
+class ActionUtilSpec extends AnyWordSpec with Matchers {
+
+  "addTrailingSlash" should {
+    "add a slash if not present in path" in {
+        addTrailingSlash("http://x.y/foo") shouldBe "http://x.y/foo/"
+    }
+
+    "don't change the url if already present in path" in {
+      addTrailingSlash("http://x.y/foo/") shouldBe "http://x.y/foo/"
+    }
+
+    "preseve parameters and fragment when adding trailing slash" in {
+      addTrailingSlash("http://x.y/foo?a=b&b=c") shouldBe "http://x.y/foo/?a=b&b=c"
+      addTrailingSlash("http://x.y/foo#bar") shouldBe "http://x.y/foo/#bar"
+      addTrailingSlash("http://x.y/foo?a=b&b=c#bar") shouldBe "http://x.y/foo/?a=b&b=c#bar"
+    }
+
+    "preseve parameters and fragment when not adding trailing slash" in {
+      addTrailingSlash("http://x.y/foo/?a=b&b=c") shouldBe "http://x.y/foo/?a=b&b=c"
+      addTrailingSlash("http://x.y/foo/#bar") shouldBe "http://x.y/foo/#bar"
+      addTrailingSlash("http://x.y/foo/?a=b&b=c#bar") shouldBe "http://x.y/foo/?a=b&b=c#bar"
+    }
+
+    "ignore trailing slash on parameters or fragment when determining if slash should be added" in {
+      addTrailingSlash("http://x.y/foo?a=b&b=c/") shouldBe "http://x.y/foo/?a=b&b=c/"
+      addTrailingSlash("http://x.y/foo#bar/") shouldBe "http://x.y/foo/#bar/"
+      addTrailingSlash("http://x.y/foo?a=b&b=c#bar/") shouldBe "http://x.y/foo/?a=b&b=c#bar/"
+    }
+  }
+
+}
