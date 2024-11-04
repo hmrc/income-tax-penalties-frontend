c1745b8611102582013d8ffa18fba3d4d81251a3
muhammadaliahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2024-11-04 16:28:33 +0000

    main template created

--- app/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/ServiceController.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/ServiceController.scala
@@ -19,15 +19,20 @@ package uk.gov.hmrc.incometaxpenaltiesfrontend.controllers
 import uk.gov.hmrc.incometaxpenaltiesfrontend.views.html.HelloWorldPage
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.incometaxpenaltiesfrontend.config.AppConfig
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
 
 @Singleton
 class HelloWorldController @Inject()(mcc: MessagesControllerComponents,
-                                     helloWorldPage: HelloWorldPage) extends FrontendController(mcc) {
+                                     helloWorldPage: HelloWorldPage)(appConfig: AppConfig) extends FrontendController(mcc) {
 
   val helloWorld: Action[AnyContent] = Action.async { implicit request =>
     Future.successful(Ok(helloWorldPage()))
+  }
+
+  val serviceSignout: Action[AnyContent] = Action {
+    Redirect(appConfig.survey).withNewSession
   }
 
 }
