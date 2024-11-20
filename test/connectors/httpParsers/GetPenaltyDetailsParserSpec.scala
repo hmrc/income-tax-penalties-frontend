97ec60c59f2787bcebee5db23d51cb2828b26654
muhammadaliahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2024-11-20 11:01:00 +0000

    controller test added

---
+++ test/connectors/httpParsers/GetPenaltyDetailsParserSpec.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.httpParsers
+
+import base.LogCapturing
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.http.Status
+import play.api.http.Status.{IM_A_TEAPOT, INTERNAL_SERVER_ERROR}
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.HttpResponse
+import uk.gov.hmrc.incometaxpenaltiesfrontend.connectors.httpParsers.{BadRequest, GetPenaltyDetailsParser, InvalidJson, UnexpectedFailure}
+import uk.gov.hmrc.incometaxpenaltiesfrontend.models.GetPenaltyDetails
+import uk.gov.hmrc.incometaxpenaltiesfrontend.utils.Logger.logger
+import uk.gov.hmrc.incometaxpenaltiesfrontend.utils.PagerDutyHelper.PagerDutyKeys
+
+class GetPenaltyDetailsParserSpec extends AnyWordSpec with Matchers with LogCapturing {
+
+  val mockGetPenaltyDetailsModel: GetPenaltyDetails = GetPenaltyDetails(
+    totalisations = None,
+    lateSubmissionPenalty = None,
+    latePaymentPenalty = None,
+    breathingSpace = None
+  )
+
+  val mockOKHttpResponseWithValidBody: HttpResponse = HttpResponse.apply(status = Status.OK, json = Json.toJson(mockGetPenaltyDetailsModel), headers = Map.empty)
+
+  val mockOKHttpResponseWithInvalidBody: HttpResponse =
+    HttpResponse.apply(status = Status.OK, json = Json.parse(
+      """
+        |{
+        | "lateSubmissionPenalty": {
+        |   "summary": {
+        |     "activePenaltyPoints": 1
+        |     }
+        |   }
+        | }
+        |""".stripMargin
+    ), headers = Map.empty)
+
+  val mockISEHttpResponse: HttpResponse = HttpResponse.apply(status = Status.INTERNAL_SERVER_ERROR, body = "Something went wrong.")
+  val mockBadRequestHttpResponse: HttpResponse = HttpResponse.apply(status = Status.BAD_REQUEST, body = "Bad Request.")
+  val mockNoContentHttpResponse: HttpResponse = HttpResponse.apply(status = Status.NO_CONTENT, body = "")
+
+  val mockImATeapotHttpResponse: HttpResponse = HttpResponse.apply(status = Status.IM_A_TEAPOT, body = "I'm a teapot.")
+
+  "GetPenaltyDetailsResponseReads" should {
+    s"parse an OK (${Status.OK}) response" when {
+      s"the body of the response is valid" in {
+        val result = GetPenaltyDetailsParser.GetPenaltyDetailsResponseReads.read("GET", "/", mockOKHttpResponseWithValidBody)
+        result.isRight shouldBe true
+        result.getOrElse(None) shouldBe mockGetPenaltyDetailsModel
+      }
+
+      s"the body is malformed - returning a $Left $InvalidJson" in {
+        withCaptureOfLoggingFrom(logger) {
+          logs => {
+            val result = GetPenaltyDetailsParser.GetPenaltyDetailsResponseReads.read("GET", "/", mockOKHttpResponseWithInvalidBody)
+            logs.exists(_.getMessage.contains(PagerDutyKeys.INVALID_JSON_RECEIVED_FROM_PENALTIES_BACKEND.toString)) shouldBe true
+            result.isLeft shouldBe true
+          }
+        }
+      }
+    }
+
+    s"parse a BAD REQUEST (${Status.BAD_REQUEST}) response - logging a PagerDuty" in {
+      withCaptureOfLoggingFrom(logger) {
+        logs => {
+          val result = GetPenaltyDetailsParser.GetPenaltyDetailsResponseReads.read("GET", "/", mockBadRequestHttpResponse)
+          logs.exists(_.getMessage.contains(PagerDutyKeys.RECEIVED_4XX_FROM_PENALTIES_BACKEND.toString)) shouldBe true
+          result.isLeft shouldBe true
+          result.left.getOrElse(UnexpectedFailure(IM_A_TEAPOT, "")) shouldBe BadRequest
+        }
+      }
+    }
+
+    s"parse a NO CONTENT (${Status.NO_CONTENT}) response" in {
+      val result = GetPenaltyDetailsParser.GetPenaltyDetailsResponseReads.read("GET", "/", mockNoContentHttpResponse)
+      result.isRight shouldBe true
+      result.getOrElse(UnexpectedFailure(IM_A_TEAPOT, "")) shouldBe mockGetPenaltyDetailsModel
+    }
+
+    s"parse an unknown error (e.g. IM A TEAPOT - ${Status.IM_A_TEAPOT}) - and log a PagerDuty" in {
+      withCaptureOfLoggingFrom(logger) {
+        logs => {
+          val result = GetPenaltyDetailsParser.GetPenaltyDetailsResponseReads.read("GET", "/", mockImATeapotHttpResponse)
+          logs.exists(_.getMessage.contains(PagerDutyKeys.RECEIVED_4XX_FROM_PENALTIES_BACKEND.toString)) shouldBe true
+          result.isLeft shouldBe true
+          result.left.getOrElse(UnexpectedFailure(INTERNAL_SERVER_ERROR, "")) shouldBe UnexpectedFailure(Status.IM_A_TEAPOT, s"Unexpected response, status ${Status.IM_A_TEAPOT} returned")
+        }
+      }
+    }
+
+    s"parse an unknown error (e.g. ISE - ${Status.INTERNAL_SERVER_ERROR}) - and log a PagerDuty" in {
+      withCaptureOfLoggingFrom(logger) {
+        logs => {
+          val result = GetPenaltyDetailsParser.GetPenaltyDetailsResponseReads.read("GET", "/", mockISEHttpResponse)
+          logs.exists(_.getMessage.contains(PagerDutyKeys.RECEIVED_5XX_FROM_PENALTIES_BACKEND.toString)) shouldBe true
+          result.isLeft shouldBe true
+          result.left.getOrElse(UnexpectedFailure(IM_A_TEAPOT, "")) shouldBe UnexpectedFailure(Status.INTERNAL_SERVER_ERROR, s"Unexpected response, status ${Status.INTERNAL_SERVER_ERROR} returned")
+        }
+      }
+    }
+  }
+}
---
+++ test/testUtils/AuthMocks.scala
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package testUtils
+
+import base.SpecBase
+import uk.gov.hmrc.incometaxpenaltiesfrontend.controllers.predicates.AuthPredicate
+import org.mockito.Matchers
+import org.mockito.Mockito.when
+import org.mockito.stubbing.OngoingStubbing
+import org.scalatest.matchers.should.Matchers
+import uk.gov.hmrc.auth.core._
+import uk.gov.hmrc.auth.core.retrieve.{Retrieval, ~}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+trait AuthMocks extends SpecBase  {
+  def setupAuthResponse(authResult: Future[~[Option[AffinityGroup], Enrolments]]): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] = {
+    when(mockAuthConnector.authorise[~[Option[AffinityGroup], Enrolments]](
+      Matchers.any(), Matchers.any[Retrieval[~[Option[AffinityGroup], Enrolments]]]())(
+      Matchers.any(), Matchers.any())
+    ).thenReturn(authResult)
+  }
+
+  val mockAuthPredicate: AuthPredicate =
+    new AuthPredicate(
+      messagesApi,
+      mcc,
+      mockAuthService,
+      errorHandler,
+      unauthorised
+    )
+
+  def mockOrganisationAuthorised(): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] =
+    setupAuthResponse(Future.successful(
+      new ~(Some(AffinityGroup.Organisation),
+        Enrolments(
+          Set(
+            Enrolment("HMRC-MTD-VAT",
+              Seq(
+                EnrolmentIdentifier("VRN", vrn)
+              ),
+              "Activated"
+            )
+          ))
+      )
+    ))
+
+  def mockAgentAuthorised(): OngoingStubbing[Future[Enrolments]] = {
+    when(mockAuthConnector.authorise[Enrolments](
+      Matchers.any(), Matchers.any())(
+      Matchers.any(), Matchers.any())
+    ).thenReturn(Future.successful(
+      Enrolments(
+        Set(
+          Enrolment(
+            "HMRC-AS-AGENT",
+            Seq(EnrolmentIdentifier("AgentReferenceNumber", "1234567")),
+            "Activated"
+          )
+        ))
+    ))
+  }
+
+  def mockAgentAuthorisedNoARN(): OngoingStubbing[Future[Enrolments]] = {
+    when(mockAuthConnector.authorise[Enrolments](
+      Matchers.any(), Matchers.any())(
+      Matchers.any(), Matchers.any())
+    ).thenReturn(Future.successful(
+      Enrolments(
+        Set(
+          Enrolment(
+            "HMRC-AS-AGENT",
+            Seq(),
+            "Activated"
+          )
+        ))
+    ))
+  }
+
+  def mockOrganisationNonActivatedMTDVATEnrolment(): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] =
+    setupAuthResponse(Future.successful(
+      new ~(Some(AffinityGroup.Organisation),
+        Enrolments(
+          Set(
+            Enrolment("HMRC-MTD-VAT",
+              Seq(
+                EnrolmentIdentifier("VRN", vrn)
+              ),
+              "Not Activated"
+            )
+          )
+        )
+      )
+    ))
+
+  def mockNoAffinityGroup(): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] =
+    setupAuthResponse(Future.successful(
+      new ~(None,
+        Enrolments(
+          Set(
+            Enrolment("HMRC-MTD-VAT",
+              Seq(
+                EnrolmentIdentifier("VRN", vrn)
+              ),
+              "Not Activated"
+            )
+          )
+        )
+      )
+    ))
+
+  def mockNoActiveSession(): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] =
+    setupAuthResponse(Future.failed(
+      MissingBearerToken("No token to be found here.")
+    ))
+
+  def mockAuthorisationException(): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] =
+    setupAuthResponse(Future.failed(
+      InternalError("There has been a mystical error.")
+    ))
+
+  def mockOrganisationNoEnrolments(): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] =
+    setupAuthResponse(Future.successful(
+      new ~(Some(AffinityGroup.Organisation),
+        Enrolments(
+          Set()
+        )
+      )
+    ))
+
+  def mockOrganisationAuthorisedWithNonRelatedEnrolments(): OngoingStubbing[Future[~[Option[AffinityGroup], Enrolments]]] =
+    setupAuthResponse(Future.successful(
+      new ~(Some(AffinityGroup.Organisation),
+        Enrolments(
+          Set(
+            Enrolment("IR-SA",
+              Seq(
+                EnrolmentIdentifier("UTR", "123456789")
+              ),
+              "Activated"),
+            Enrolment("HMRC-MTD-VAT",
+              Seq(
+                EnrolmentIdentifier("VRN", vrn)
+              ),
+              "Activated"
+            ),
+            Enrolment("IR-CT",
+              Seq(
+                EnrolmentIdentifier("UTR", "123456789")
+              ),
+              "Activated")
+          ))
+      )
+    ))
+}
