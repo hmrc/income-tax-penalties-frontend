5b6ccece33b66423461018b57fcd9a93db833a3c
Muhammad-Ali-Ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2025-02-14 17:13:29 +0000

    MIPR-1525: Non breaking spaces added to prevent dates from overflowing to next line (#27)
    * non breaking spaces added to dates
    
    * code refactored

--- app/uk/gov/hmrc/incometaxpenaltiesfrontend/utils/DateFormatter.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/utils/DateFormatter.scala
@@ -22,11 +22,28 @@ import java.time.LocalDate
 
 trait DateFormatter {
 
-  def dateToString(date: LocalDate)(implicit messages: Messages): String =
-    s"${date.getDayOfMonth} ${messages(s"month.${date.getMonthValue}")} ${date.getYear}"
+  def dateToString(date: LocalDate)(implicit messages: Messages): String = {
+    val dateNonBreaking = s"${date.getDayOfMonth} ${messages(s"month.${date.getMonthValue}")} ${date.getYear}"
+    dateNonBreaking.replace(" ", "\u00A0")
+  }
 
-  def dateToMonthYearString(date: LocalDate)(implicit messages: Messages): String =
-    s"${messages(s"month.${date.getMonthValue}")} ${date.getYear}"
+  def dateToMonthYearString(date: LocalDate)(implicit messages: Messages): String = {
+    val dateNonBreaking = s"${messages(s"month.${date.getMonthValue}")} ${date.getYear}"
+    dateNonBreaking.replace(" ", "\u00A0")
+  }
+
+  def dateNonBreakingSpaceMultiple(key: String, from: String, to: String)(implicit messages: Messages): String =
+    messages(
+      key,
+      htmlNonBroken(from).format(from),
+      htmlNonBroken(to).format(to)
+    )
+
+  def dateNonBreakingSpaceSingle(date: String)(implicit messages: Messages): String = htmlNonBroken(date).format(date)
+
+  def htmlNonBroken(string: String): String =
+    string.replace(" ", "\u00A0")
+
 }
 
 object DateFormatter extends DateFormatter
