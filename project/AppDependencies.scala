e0c70f1a6bcaf00a74c17bec477b18bcdaec6b56
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-03-25 09:48:13 +0000

    [MIPR-1704] Bobby violations and other dependency bumps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,17 +5,17 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.5.0"
+  private val bootstrapVersion = "9.11.0"
   private val playVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"    %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc"    %% s"play-frontend-hmrc-$playVersion" % "11.7.0"
+    "uk.gov.hmrc"    %% s"play-frontend-hmrc-$playVersion" % "12.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup" % "jsoup" % "1.18.3" % Test,
-    "uk.gov.hmrc"     %%   "bootstrap-test-play-30" % bootstrapVersion % Test
+    "org.jsoup"       %    "jsoup"                  % "1.19.1"          % Test,
+    "uk.gov.hmrc"     %%   "bootstrap-test-play-30" % bootstrapVersion  % Test
   )
 
   val it: Seq[Nothing] = Seq.empty
