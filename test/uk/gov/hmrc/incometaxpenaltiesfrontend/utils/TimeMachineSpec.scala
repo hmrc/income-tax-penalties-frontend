61d3d9307fb1650596ea3e43b9dd5e7b0c192bed
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2025-05-15 16:41:36 +0100

    MIPR-1684: add TimeMachineSpec

--- test/uk/gov/hmrc/incometaxpenaltiesfrontend/utils/TimeMachineSpec.scala
+++ test/uk/gov/hmrc/incometaxpenaltiesfrontend/utils/TimeMachineSpec.scala
@@ -1,5 +1,48 @@
 package uk.gov.hmrc.incometaxpenaltiesfrontend.utils
 
-class TimeMachineSpec {
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.inject.guice.GuiceApplicationBuilder
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class TimeMachineSpec extends AnyWordSpec with Matchers{
+
+  private val dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+
+
+  "TimeMachine -getCurrentDate" should {
+
+    "Return the configured date specified" in {
+      val app = new GuiceApplicationBuilder().configure(
+        "timemachine.enabled" -> true,
+        "timemachine.date" -> "01-01-2021"
+      ).build()
+
+      val timeMachine = app.injector.instanceOf[TimeMachine]
+      timeMachine.getCurrentDate shouldEqual LocalDate.parse("01-01-2021", dateFormatter)
+    }
+  }
+
+  "Return the current date if timemachine is disabled" in {
+    val app = new GuiceApplicationBuilder().configure(
+      "timemachine.enabled" -> false
+    ).build()
+
+    val timeMachine = app.injector.instanceOf[TimeMachine]
+    timeMachine.getCurrentDate shouldEqual LocalDate.now()
+  }
+
+  "Return the current date if timemachine date set to now" in {
+
+    val app = new GuiceApplicationBuilder().configure(
+      "timemachine.enabled" -> true,
+      "timemachine.date" -> "now"
+    ).build()
+
+    val timeMachine = app.injector.instanceOf[TimeMachine]
+    timeMachine.getCurrentDate shouldEqual LocalDate.now()
+
+  }
 }
