6c0342636d8afabc75e5176101dde074a3832236
nimo-mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-06-26 12:19:24 +0100

    MIPR-1854: Fix link for appeal start

--- app/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/AppealsController.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/AppealsController.scala
@@ -39,11 +39,7 @@ class AppealsController @Inject()(val authActions: AuthActions,
     authActions.asMTDUser(isAgent) { _ =>
       logger.debug(s"[IndexController][redirectToAppeals] - Redirect to appeals frontend with id $penaltyId and is late payment penalty: $isLPP " +
         s"and cannot be appealed: $isFindOutHowToAppealLSP and is LPP2: $isLPP2")
-      if (isFindOutHowToAppealLSP) {
-        Redirect(s"${appConfig.incomeTaxPenaltiesAppealsBaseUrl}" + s"/appeal-start")
-      } else {
-        Redirect(s"${appConfig.incomeTaxPenaltiesAppealsBaseUrl}/initialise-appeal?penaltyId=$penaltyId&isAgent=$isAgent&isLPP=$isLPP&isAdditional=$isLPP2&is2ndStageAppeal=$is2ndStageAppeal")
-      }
+      Redirect(s"${appConfig.incomeTaxPenaltiesAppealsBaseUrl}/initialise-appeal?penaltyId=$penaltyId&isAgent=$isAgent&isLPP=$isLPP&isAdditional=$isLPP2&is2ndStageAppeal=$is2ndStageAppeal")
     }
 
 
