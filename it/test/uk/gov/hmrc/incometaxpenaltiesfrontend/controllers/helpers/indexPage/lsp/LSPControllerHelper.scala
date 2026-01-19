ca70968450a7041bfa8254b1d68f2b4879ffa6fb
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-01-19 09:28:51 +0000

    Create a new object for AB111131A

--- it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/helpers/indexPage/lsp/LSPControllerHelper.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/helpers/indexPage/lsp/LSPControllerHelper.scala
@@ -60,8 +60,8 @@ trait LSPControllerHelper extends ControllerISpecHelper {
       "AB111120A" -> AB111120A,
       "AB111121A" -> AB111121A,
       "AB111122A" -> AB111122A,
-      "AB111130A" -> AB111130A
-//      "AB111131A" -> AB111131A,
+      "AB111130A" -> AB111130A,
+      "AB111131A" -> AB111131A
 //      "AB111132A" -> AB111132A,
 //      "AB121110A" -> AB121110A
     )
