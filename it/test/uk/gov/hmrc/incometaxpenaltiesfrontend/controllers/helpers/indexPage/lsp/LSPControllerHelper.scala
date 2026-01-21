0edda2dee5a40aa502c22edaf4c409ba1f782f4c
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-01-21 17:55:13 +0000

    Create a new object in the lsp folder for nino PE000003A

--- it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/helpers/indexPage/lsp/LSPControllerHelper.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/helpers/indexPage/lsp/LSPControllerHelper.scala
@@ -71,8 +71,8 @@ trait LSPControllerHelper extends ControllerISpecHelper {
       "AB400020A" -> AB400020A,
       "AB411110A" -> AB411110A,
       "AB411145A" -> AB411145A,
-      "PE000001A" -> PE000001A
-//      "PE000003A" -> PE000003A
+      "PE000001A" -> PE000001A,
+      "PE000003A" -> PE000003A
     )
   }
 
