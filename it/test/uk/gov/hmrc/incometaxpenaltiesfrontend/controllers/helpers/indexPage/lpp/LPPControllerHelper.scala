8a4c6fd96993ef7994b6164965849a72700c0388
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-01-28 10:41:49 +0000

    Create a new object in LPP for AA100002C

--- it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/helpers/indexPage/lpp/LPPControllerHelper.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/controllers/helpers/indexPage/lpp/LPPControllerHelper.scala
@@ -29,7 +29,7 @@ trait LPPControllerHelper extends ControllerISpecHelper {
     "AA100000B" -> AA100000B,
     "AA100000C" -> AA100000C,
     "AA100000D" -> AA100000D,
-//    "AA100002C" -> AA100002C,
+    "AA100002C" -> AA100002C,
 //    "AA123450A" -> AA123450A,
 //    "AA200000A" -> AA200000A,
 //    "AA200000B" -> AA200000B,
