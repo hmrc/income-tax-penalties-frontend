8dca7fbdf2afcc9802236c5ddb88c4ec424b5fa1
kSiadat <49532979+kSiadat@users.noreply.github.com>
2026-02-23 15:46:53 +0000

    MIPR-1872: correction

--- app/uk/gov/hmrc/incometaxpenaltiesfrontend/utils/EnrolmentUtil.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/utils/EnrolmentUtil.scala
@@ -22,11 +22,11 @@ object EnrolmentUtil {
 
   val agentEnrolmentKey = "HMRC-AS-AGENT"
   val incomeTaxEnrolmentKey = "HMRC-MTD-IT"
-  val secondaryIncomeTaxEnrolmentKey = "HMRC-MTD-IT"
+  val secondaryIncomeTaxEnrolmentKey = "HMRC-MTD-IT-SUPP"
   val arnKey = "AgentReferenceNumber"
   val mtdItIdKey = "MTDITID"
   val agentDelegatedAuthRuleKey = "mtd-it-auth"
-  val secondaryAgentDelegatedAuthRuleKey = "mtd-it-auth"
+  val secondaryAgentDelegatedAuthRuleKey = "mtd-it-auth-supp"
 
   val agentDelegatedAuthorityRule: String => Enrolment =
     mtdItId =>
