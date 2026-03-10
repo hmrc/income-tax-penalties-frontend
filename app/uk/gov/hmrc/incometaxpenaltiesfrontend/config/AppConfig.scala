37e2dc776ba0df7cd31642531c9a5a57b13190c8
nadinJ <63252254+nadinJ@users.noreply.github.com>
2026-03-10 16:11:03 +0000

    MIPR-2490: Added timeMachine banner

--- app/uk/gov/hmrc/incometaxpenaltiesfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/config/AppConfig.scala
@@ -91,6 +91,10 @@ class AppConfig @Inject()(val config: Configuration, servicesConfig: ServicesCon
   lazy val timeMachineDate: String =
     config.get[String]("timemachine.date")
 
+  lazy val timeMachineBannerEnabled: Boolean =
+    config.get[Boolean]("timeMachineBanner.enabled")
+
+
   def optCurrentDate: Option[LocalDate] = {
     if(timeMachineEnabled) {
       val optDateString = sys.props.get(TIME_MACHINE_NOW).getOrElse(timeMachineDate)
