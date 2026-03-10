701cabd35a76e7160d5af4b918d8de2685b23b77
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-03-10 11:52:37 +0000

    Remove overdue overview AA100002C

--- it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/penaltyDetails/users/lpp/AA100002C.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/penaltyDetails/users/lpp/AA100002C.scala
@@ -43,7 +43,7 @@ object AA100002C extends UserDetailsData {
   )
 
   override val expectedOverviewText: Boolean => String = isAgent =>
-    s"Overview ${if (isAgent) "Your client’s" else "Your"} account has: overdue Income Tax charges a late payment penalty Check what you owe"
+    s"Overview ${if (isAgent) "Your client’s" else "Your"} account has a late payment penalty Check what you owe"
 
   override val timeMachineDate: String = "01/01/2026"
 }
