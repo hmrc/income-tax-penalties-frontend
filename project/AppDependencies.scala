3e23690ed0fb0de3886d51747b2b8346683b1a00
muhammad-ali-ahmad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2026-03-16 17:42:09 +0000

    dependency updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
   private val playVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
