16e7e047c370e07ee3d3847390ee36e71bbcbcd5
muhammad-ali-ahmad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2026-03-30 15:41:57 +0100

    review updated

--- app/uk/gov/hmrc/incometaxpenaltiesfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/config/AppConfig.scala
@@ -105,6 +105,8 @@ class AppConfig @Inject()(val config: Configuration, servicesConfig: ServicesCon
 
   val guidanceOnLSPUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#late-submission-penalties"
 
+  val guidanceOnLPPUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#late-paymentpenalties"
+  
   val moreInformationLinkUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#removing-penalty-points-and-penalties"
 
   val addedPointsGuidanceLinkUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#if-you-already-have-penalty-points"
