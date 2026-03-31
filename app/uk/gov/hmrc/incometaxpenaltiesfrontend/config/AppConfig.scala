c6efe026a1eec62dc3229bf056473d44e7293e51
Muhammad-Ali-Ahmad <muhammad.ahmad@digital.hmrc.gov.uk>
2026-03-31 09:35:47 +0100

    MIPR-1590: links and test added (#209)
    * links and test added
    
    * links and test added
    
    * refactored
    
    * review updated
    
    * review updated
    
    * review updated
    
    * tests updated
    
    * test updated

--- app/uk/gov/hmrc/incometaxpenaltiesfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/incometaxpenaltiesfrontend/config/AppConfig.scala
@@ -102,8 +102,11 @@ class AppConfig @Inject()(val config: Configuration, servicesConfig: ServicesCon
 
   lazy val agentServicesUrl: String = config.get[String]("agent-services-account-frontend.baseUrl") +"/agent-services-account/no-assignment"
 
-  val guidanceOnLPPUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#late-paymentpenalties"
 
+  val guidanceOnLSPUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#late-submission-penalties"
+
+  val guidanceOnLPPUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#late-paymentpenalties"
+  
   val moreInformationLinkUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#removing-penalty-points-and-penalties"
 
   val addedPointsGuidanceLinkUrl: String = "https://www.gov.uk/guidance/penalties-for-making-tax-digital-for-income-tax#if-you-already-have-penalty-points"
