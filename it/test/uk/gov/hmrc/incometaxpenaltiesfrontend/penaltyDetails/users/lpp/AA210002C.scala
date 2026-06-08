85f3e7a3e7f5a6588011449d7475c3884282b77a
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-06-08 16:09:14 +0100

    Update AA220002C

--- it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/penaltyDetails/users/lpp/AA210002C.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/penaltyDetails/users/lpp/AA210002C.scala
@@ -43,7 +43,7 @@ object AA210002C extends UserDetailsData {
     validateSummary(cardRows.get(0), "Pay penalty by", "17 April 2028")
     validateSummary(cardRows.get(1), "Overdue charge", "Income Tax for 2026 to 2027 tax year")
     validateSummary(cardRows.get(2), "Income Tax due", "31 January 2028")
-    validateSummary(cardRows.get(3), "Income Tax paid", "16 March 2028")
+    validateSummary(cardRows.get(3), "Income Tax paid", "Payment not yet received")
     validateViewCalculationLink(card, 1)
     validateAppealLink(card.getElementsByClass("govuk-link").get(1))
   }
