37e9264a677447908130aa3f3854e6d8b7d98818
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-08-05 08:33:10 +0100

    Update AC100002A

--- it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/penaltyDetails/users/lpp/AC100002AOverdue.scala
+++ it/test/uk/gov/hmrc/incometaxpenaltiesfrontend/penaltyDetails/users/lpp/AC100002AOverdue.scala
@@ -43,7 +43,7 @@ object AC100002AOverdue extends UserDetailsData {
     validateSummary(cardRows.get(0), "Pay penalty by", "4 April 2025")
     validateSummary(cardRows.get(1), "Overdue charge", "Income Tax for 2023 to 2024 tax year")
     validateSummary(cardRows.get(2), "Income Tax due", "31 January 2025")
-    validateSummary(cardRows.get(3), "Income Tax paid", "Payment not yet received")
+    validateSummary(cardRows.get(3), "Income Tax paid", "Payment plan agreed")
     validateViewCalculationLink(card, 1)
     validateAppealLink(card.getElementsByClass("govuk-link").get(1))
   }
@@ -56,7 +56,7 @@ object AC100002AOverdue extends UserDetailsData {
     validateSummary(cardRows.get(0), "Pay penalty by", "4 April 2025")
     validateSummary(cardRows.get(1), "Overdue charge", "Income Tax for 2023 to 2024 tax year")
     validateSummary(cardRows.get(2), "Income Tax due", "31 January 2025")
-    validateSummary(cardRows.get(3), "Income Tax paid", "Payment not yet received")
+    validateSummary(cardRows.get(3), "Income Tax paid", "Payment plan agreed")
     validateViewCalculationLink(card, 2)
     validateAppealLink(card.getElementsByClass("govuk-link").get(1))
   }
