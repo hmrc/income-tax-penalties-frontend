9c4aeec377d8595f87c2b50b7f31cfff9620cc04
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2026-08-19 14:53:49 +0100

    update LSPOverviewMessages

--- test-fixtures/fixtures/messages/LSPOverviewMessages.scala
+++ test-fixtures/fixtures/messages/LSPOverviewMessages.scala
@@ -24,28 +24,26 @@ object LSPOverviewMessages {
 
     //Messages when points are accruing < threshold
     val pointsAccruingP1: Int => String = {
-      case 1 => "You have 1 penalty point for sending a late submission. You should send this missing submission as soon as possible if you haven’t already."
-      case n => s"You have $n penalty points for sending late submissions. You should send any missing submissions as soon as possible if you haven’t already."
+      case 1 => "You have 1 penalty point for missing a submission deadline. You must send the missing submission as soon as possible if you have not already."
+      case n => s"You have $n penalty points for missing submission deadlines. You must send the missing submissions as soon as possible if you have not already."
     }
 
-    val pointsAccruingP2: String = "You’ll get another point if you send another submission after a deadline has passed."
-
-    val pointsAccruingP3: Int => String = {
-      n => s"Points usually expire after 24 months, but it can be longer if you keep sending late submissions. If you reach $n points, you’ll have to pay a £200 penalty."
-    }
-
-    val pointsAccruingWarning: String = "! Warning You’ll get a £200 penalty if you send another late submission."
+    val pointsAccruingP2: String = "You’ll get another point if you miss a submission deadline again."
+    val pointsAccruingP3: Int => String = threshold => s"Points usually last for 24 months. They can last longer if you keep missing submission deadlines. If you get $threshold points, you’ll have to pay a £200 penalty."
+    val pointsAccruingP3Special : String = "Points usually last for 24 months. They can last longer if you keep missing submission deadlines."
+    val pointsAccruingWarning: Int => String = threshold =>  s"! Warning If you get $threshold points, you’ll have to pay a £200 penalty"
 
     //Messages when points == threshold (1 financial penalty triggered)
-    val penaltyP1: String = "You will get an additional £200 penalty every time you send a late submission in the future, until your points are removed. You should send any missing submissions as soon as possible if you haven’t already."
-    val penaltyWarning: String = "! Warning You have been given a £200 penalty for reaching the penalty threshold."
+    val penaltyP1: String = "You’ll get another £200 penalty every time you miss a submission deadline until your penalty points are removed."
+    val penaltyP2: String = "You must send the missing submissions as soon as possible if you have not already."
+    val penaltyWarning: String = "! Warning You have a £200 penalty to pay because you have 4 penalty points."
 
     //Messages when points == threshold (more than one financial penalty triggered)
-    val additionalPenaltyP1: String = "You will get another £200 penalty every time you send a late submission in the future, until your points are removed. You should send any missing submissions as soon as possible if you haven’t already."
-    val additionalPenaltyWarning: String = "! Warning You have been given an additional £200 penalty."
-    val pointsGuidanceLink = "Read the guidance about late submission penalties"
+    val additionalPenaltyP1: String = "You’ll get another £200 penalty every time you miss a submission deadline until your penalty points are removed."
+    val additionalPenaltyWarning: String = "! Warning You have another £200 penalty to pay"
+    val pointsGuidanceLink = "Find out more about late submission penalties"
     val addedPointsGuidanceLink = "Read the guidance about adjustment points"
-    val actionsLink: String = "Actions you must take to get your points removed by April 2028"
+    val actionsLink: String = "Find out how to get your penalty points removed"
   }
   
   object English extends Messages with En
@@ -55,27 +53,27 @@ object LSPOverviewMessages {
 
     //Messages when points are accruing < threshold
     override val pointsAccruingP1: Int => String = {
-      case 1 => "Mae gennych 1 pwynt cosb am gyflwyno’n hwyr. Dylech anfon y cyflwyniad sydd ar goll cyn gynted â phosibl os nad ydych eisoes wedi gwneud hynny."
-      case n => s"Mae gennych $n o bwyntiau cosb am gyflwyno’n hwyr. Dylech anfon unrhyw gyflwyniadau sydd ar goll cyn gynted â phosibl os nad ydych eisoes wedi gwneud hynny."
+      case 1 => "Mae gennych 1 pwynt cosb am fethu terfyn amser cyflwyno. Mae’n rhaid i chi anfon y cyflwyniad sydd ar goll cyn gynted â phosibl os nad ydych wedi gwneud hynny eisoes."
+      case n => s"Mae gennych $n bwynt cosb am fethu terfynau amser cyflwyno. Mae’n rhaid i chi anfon y cyflwyniadau sydd ar goll cyn gynted â phosibl os nad ydych wedi gwneud hynny eisoes."
     }
     
-    override val pointsAccruingP2: String = "Byddwch yn cael pwynt arall os byddwch yn anfon cyflwyniad arall ar ôl i’r dyddiad cau fynd heibio."
+    override val pointsAccruingP2: String = "Byddwch yn cael pwynt arall os byddwch yn methu terfyn amser cyflwyno eto."
+
+    override val pointsAccruingP3: Int => String = threshold => s"Mae pwyntiau fel arfer yn para am 24 mis. Gallant barhau’n hirach os byddwch yn parhau i fethu terfynau amser cyflwyno. Os byddwch yn cael $threshold pwynt, bydd angen i chi dalu cosb o £200."
+    override val pointsAccruingP3Special : String = "Mae pwyntiau fel arfer yn para am 24 mis. Gallant barhau’n hirach os byddwch yn parhau i fethu terfynau amser cyflwyno."
     
-    override val pointsAccruingP3: Int => String = {
-        n => s"Mae pwyntiau fel arfer yn cael eu dileu ar ôl 24 mis, ond gall fod yn hirach os ydych yn parhau i anfon cyflwyniadau’n hwyr. Os ydych yn cyrraedd $n o bwyntiau, bydd angen i chi dalu cosb o £200."
-    }
-    
-    override val pointsAccruingWarning: String = "! Warning Bydd cosb o £200 yn cael ei chodi arnoch os byddwch yn anfon cyflwyniad arall yn hwyr."
+    override val pointsAccruingWarning: Int => String = threshold => s"! Warning Os byddwch yn cael $threshold pwynt, bydd angen i chi dalu cosb o £200."
     
     //Messages when points == threshold (1 penalty triggered)
-    override val penaltyP1: String = "Codir cosb ychwanegol o £200 arnoch bob tro y byddwch yn anfon cyflwyniad hwyr yn y dyfodol, hyd nes y bydd pob un o’ch pwyntiau wedi’u dileu. Dylech anfon unrhyw gyflwyniadau sydd ar goll cyn gynted â phosibl os nad ydych eisoes wedi gwneud hynny."
-    override val penaltyWarning: String = "! Warning Codwyd cosb o £200 arnoch oherwydd eich bod wedi cyrraedd y trothwy ar gyfer pwyntiau."
+    override val penaltyP1: String = "Byddwch yn cael cosb arall o £200 bob tro y byddwch yn methu terfyn amser cyflwyno nes bod eich pwyntiau cosb yn cael eu dileu."
+    override val penaltyP2: String = "Mae’n rhaid i chi anfon y cyflwyniadau sydd ar goll cyn gynted â phosibl os nad ydych wedi gwneud hynny eisoes."
+    override val penaltyWarning: String = "! Warning Mae gennych gosb o £200 i’w thalu oherwydd fod gennych 4 pwynt cosb."
 
     //Messages when points == threshold (more than one financial penalty triggered)
-    override val additionalPenaltyP1: String = "Codir cosb ychwanegol o £200 arnoch bob tro y byddwch yn anfon cyflwyniad yn hwyr yn y dyfodol, hyd nes y bydd pob un o’ch pwyntiau wedi’u dileu. Dylech anfon unrhyw gyflwyniadau sydd ar goll cyn gynted â phosibl os nad ydych eisoes wedi gwneud hynny."
-    override val additionalPenaltyWarning: String = "! Warning Codwyd cosb ychwanegol o £200 arnoch."
-    override val pointsGuidanceLink = "Darllenwch yr arweiniad am gosbau am dalu’n hwyr"
+    override val additionalPenaltyP1: String = "Byddwch yn cael cosb arall o £200 bob tro y byddwch yn methu terfyn amser cyflwyno nes bod eich pwyntiau cosb yn cael eu dileu."
+    override val additionalPenaltyWarning: String = "! Warning Mae gennych gosb arall o £200 i’w thalu"
+    override val pointsGuidanceLink = "Dysgwch ragor am gosbau am dalu’n hwyr"
     override val addedPointsGuidanceLink = "Darllenwch yr arweiniad ynghylch pwyntiau addasu"
-    override val actionsLink: String = "Y camau i’w cymryd i gael eich pwyntiau wedi’u dileu erbyn mis Ebrill 2028"
+    override val actionsLink: String = "Dysgwch sut i gael gwared ar eich pwyntiau cosb"
   }
 }
