470f7207eefda964fdd8b506eac7cc885a8ba702
nadinJ <63252254+nadinJ@users.noreply.github.com>
2026-08-26 17:06:13 +0100

    MIPR-2941: fixed ninos

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
   private val playVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"    %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc"    %% s"play-frontend-hmrc-$playVersion" % "13.9.0"
+    "uk.gov.hmrc"    %% s"play-frontend-hmrc-$playVersion" % "13.11.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup"       %    "jsoup"                  % "1.22.2"          % Test,
+    "org.jsoup"       %    "jsoup"                  % "1.23.2"          % Test,
     "uk.gov.hmrc"     %%   "bootstrap-test-play-30" % bootstrapVersion  % Test,
-    "org.scalamock"   %% "scalamock"                % "7.5.5"           % Test,
+    "org.scalamock"   %%   "scalamock"              % "7.5.5"           % Test,
   )
 
   val it: Seq[Nothing] = Seq.empty
